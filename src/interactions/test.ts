0ce7da9482600a09425f02a17a3191aef5cc4304
Manaciaa <150796156+Manaciaa@users.noreply.github.com>
2024-10-10 11:12:15 +0200

    Add files via upload

---
+++ src/interactions/test.ts
@@ -0,0 +1,30 @@
+"use strict";
+
+/* const headerTag = document.getElementsByTagName('header') as HTMLCollection <HTMLElement>;
+
+// Change content 
+
+headerTag.innerText = "Elden Header"
+headerTag.innerHTML = "<p> Ceci est une modification du header </p>"
+
+// Change Style 
+
+headerTag.style.color = "blue";
+headerTag.style.background = "black";
+headerTag.style.border = "1px solid black";
+headerTag.style.margin = "10px";
+headerTag.style.padding = "50vh";
+
+// Change classes
+
+headerTag.classList.add('dark-mode');
+headerTag.classList.add('gaming-background gaming-border');
+headerTag.classList.remove('gaming-border');
+headerTag.classList.toggle('dark-mode');
+
+
+
+
+
+
+*/
